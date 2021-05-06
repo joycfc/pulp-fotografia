8b590d8a18595705113f9f9114fd0b9938875dcb
Joao franco <joaocfranco81@gmail.com>
2021-05-06 15:01:05 -0300

    subida al repositorio de github

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+/*--menu--*/
+
+
+var menu = document.getElementById("menu");
+
+function closemenu() {
+
+    menu.style.top = "-100vh";
+}
+
+function openmenu() {
+
+    menu.style.top = "0";
+}
+
+/*--menu--*/
+
+/*--galeria--*/
+
+document.addEventListener("click", function (e) {
+	if (e.target.classList.contains("gallery-item")) {
+		const src = e.target.getAttribute("src");
+		document.querySelector(".modal-img").src = src;
+		const myModal = new bootstrap.Modal(document.getElementById('modalGaleria'));
+		myModal.show();
+	}
+})
+
+/*--galeria--*/
+
+
